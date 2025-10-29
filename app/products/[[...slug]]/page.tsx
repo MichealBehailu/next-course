49cdb8abc63b1620c9fae9c7527c2c91a604cf5c
Micheal <michealbehailu94@gmail.com>
2025-10-29 16:14:39 +0300

    another

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 
 interface Props {
-  params: { slug: string[] };
-  searchParams: {sortOrder: string}
+  params: { slug?: string[] };
+  searchParams: { sortOrder?: string };
 }
 
-const ProductPage = ({ params: { slug }, searchParams:{sortOrder} }: Props) => {
-  return <div>ProductPage {slug} {sortOrder}</div>;
+const ProductPage = ({
+  params: { slug },
+  searchParams: { sortOrder },
+}: Props) => {
+  return (
+    <div>
+      ProductPage {slug && slug.join("/")} {sortOrder}
+    </div>
+  );
 };
 
 export default ProductPage;
 
-//the filename [[...slug]] the double bracket indicates it is optional plus it has a benfit to handle nested endpoints 
+//the filename [[...slug]] the double bracket indicates it is optional plus it has a benfit to handle nested endpoints
