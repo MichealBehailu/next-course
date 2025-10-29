000180ef309619ef7906f9c7b1cb525ffd0a9b40
Micheal <michealbehailu94@gmail.com>
2025-10-29 15:13:38 +0300

    Fixing : param

--- app/api/products/[id]/route.tsx
+++ app/api/products/[id]/route.tsx
@@ -1,17 +1,15 @@
-import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
 import { prisma } from "@/prisma/client";
-import { id } from "zod/locales";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
-  //   const body = await request.json();
+  const { id } = await context.params;
 
   const product = await prisma.product.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: parseInt(id) },
   });
 
   if (!product)
@@ -25,17 +23,17 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await context.params;
   const body = await request.json();
   const validation = schema.safeParse(body);
 
-  if (!validation.success) return NextResponse.json(validation.error.issues);
+  if (!validation.success)
+    return NextResponse.json(validation.error.issues, { status: 400 });
 
   const product = await prisma.product.findUnique({
-    where: {
-      id: parseInt(params.id),
-    },
+    where: { id: parseInt(id) },
   });
 
   if (!product)
@@ -45,9 +43,7 @@ export async function PUT(
     );
 
   const updatedProduct = await prisma.product.update({
-    where: {
-      id: product.id,
-    },
+    where: { id: product.id },
     data: {
       name: body.name,
       price: body.price,
@@ -59,14 +55,15 @@ export async function PUT(
 
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await context.params;
+
   const product = await prisma.product.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: parseInt(id) },
   });
 
   if (!product)
-    //if product doesnt exist
     return NextResponse.json(
       { error: "Product doesn't exist!" },
       { status: 404 }
@@ -76,5 +73,5 @@ export async function DELETE(
     where: { id: product.id },
   });
 
-  return NextResponse.json({}, { status: 200 }); //return NextResponse.json({message : "Product sccessfully deleted"}, {status : 200})
+  return NextResponse.json({}, { status: 200 });
 }
